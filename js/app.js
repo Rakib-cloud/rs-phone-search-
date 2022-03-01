b321c268fcadf37bd26d2806824b05c551ae8078
Rakib-cloud <ri1641715@gmail.com>
2022-03-01 22:39:51 +0600

    no phone found work update

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ const allphones=() => {
     //step2:api fetching from api link
     const url=`https://openapi.programming-hero.com/api/phones?search=${searchvalue}`;
     fetch(url).then(res=>res.json()
-        ).then(data=>showphonedetails(data.data));//array finding form object
+        ).then(data=>showphonedetails(data.data.slice(0,20)));//array finding form object and show only 20 data with slice method
      
 }
 
@@ -18,35 +18,34 @@ const allphones=() => {
 const showphonedetails=(phones)=>{
  //console.log(phones);
  const searchvalue=document.getElementById('search-box').value;
-
+let flag=0;
  for(const phone of phones){
     //console.log(phones);
     //if phone dont match alert showing not done
-    // if(searchvalue!=phone){
-    //     alert('No found');
-    // }
     const parent=document.getElementById('phone-container');
-     const div=document.createElement('div');
+    const div=document.createElement('div');
 
- div.innerHTML=`
- <div class="card border text-center">
- <div class="pro-pic">
-     <img class="w-25"src="${phone.image}" alt="">
- </div>
- <h2>Name:${phone.phone_name}</h2>
- <h5>Brand:${phone.brand}</h5>
- <p></p>
- <div class="allbutton"> 
-    <button onclick="details('${phone.slug}')" class=" btn btn-success">Details</button>
- </div>
+div.innerHTML=`
+<div class="card border text-center">
+<div class="pro-pic">
+    <img class="w-25"src="${phone.image}" alt="">
 </div>
- `;
+<h2>Name:${phone.phone_name}</h2>
+<h5>Brand:${phone.brand}</h5>
+<p></p>
+<div class="allbutton"> 
+   <button onclick="details('${phone.slug}')" class=" btn btn-success">Details</button>
+</div>
+</div>
+`;
 
- parent.appendChild(div);
-    
+parent.appendChild(div);
+   flag=flag+1;
     
  }
-
+if(flag==0){
+  alert('no phone data found');
+}
 }
 
 //details button click then show more details of the phone
@@ -80,7 +79,7 @@ const details = (id) => {
    <h5>Chipset:${info.mainFeatures.chipSet}</h5>
    <h5>Memory:${info.mainFeatures.memory}</h5>
    
-   <h5>Sensors:${info.mainFeatures.sensors[0]}</h5>
+   <h5>Sensors:${info.mainFeatures.sensors}</h5>
        </div>
      </div>
       
